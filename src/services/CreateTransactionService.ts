e3760b7d626323d2db0529b35794c7eb6e6262ca
lucianodiisouza <lucianodiisouza@hotmail.com>
2021-01-10 04:46:54 -0300

    :sparkles: challenged finalized

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,12 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error(
+        'Transaction type inserted is invalid, need to be Income or Outcome',
+      );
+    }
+
     const { total } = this.transactionsRepository.getBalance();
 
     if (type === 'outcome' && total < value) {
